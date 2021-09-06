9cd2d69beb33f0296c5f2dab2655b469889f1368
empiteChathu <imchathu87@gmail.com>
2021-09-06 10:48:54 +0530

    commit 9

---
+++ SampleEchoAR2020117f1/Assets/Scripts/TabButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public TabGroup tabGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tabGroup.Subscribe(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+}
---
+++ SampleEchoAR2020117f1/Assets/Scripts/TabGroup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public List<GameObject> pagesToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+        
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        int index = button.transform.GetSiblingIndex();
+        print(index);
+        for (int i = 0; i < pagesToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                pagesToSwap[i].SetActive(true);
+            }
+            else
+            {
+                pagesToSwap[i].SetActive(false);
+            }
+        }
+    }
+}
